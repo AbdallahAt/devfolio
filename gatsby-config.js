a4463776de7571a959305dbadb4e1b23447cfba7
abdallahat <abdallah.atouani@rwth-aachen.de>
2021-03-19 10:48:29 +0100

    add site icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,7 +78,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`, // This color appears on mobile
         display: `minimal-ui`,
-        icon: `src/images/logo.png`,
+        icon: `src/images/logo_small.png`,
       },
     },
   ],
